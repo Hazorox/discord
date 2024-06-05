c756b0455100e09537b09804ba1409b10eaf3e4c
Hamza Mohamed Awad <hamzaawad8899@gmail.com>
2024-06-05 15:40:30 +0300

    Why did I do this

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -1,12 +1,25 @@
-module.exports = {
-  name: 'ping',
-  description: 'Pong!',
-  // devOnly: Boolean,
-  testOnly: true,
-  // options: Object[],
-  // deleted: Boolean,
+const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
 
+module.exports = {
+  name: 'ban',
+  description: 'u dont wanna try :3',
+  devOnly: false,
+  testOnly: true,
+  options: [
+    {
+      name: 'targetUser',
+      description: 'try this on ur friend',
+      type: ApplicationCommandOptionType.Mentionable,
+      required: true,
+    },
+    {
+      name: 'reason',
+      description: 'try this on ur friend :)',
+      type: ApplicationCommandOptionType.String,
+    },
+  ],
+  defaultMemberPermissions: [PermissionFlagsBits.Administrator],
   callback: (client, interaction) => {
-    interaction.reply(`Pong! ${client.ws.ping}ms`);
+    interaction.reply('Pong 🏓');
   },
 };
--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,19 +1,28 @@
 const path = require('path');
-const getAllFiles = require('../utils/getAllFiles');
+const getAllFiles = require("../utils/getAllFiles");
 
 module.exports = (client) => {
-  const eventFolders = getAllFiles(path.join(__dirname, '..', 'events'), true);
+  const eventFolders = getAllFiles(path.join(__dirname, '../events'), true);
 
   for (const eventFolder of eventFolders) {
     const eventFiles = getAllFiles(eventFolder);
-    eventFiles.sort((a, b) => a > b);
+    eventFiles.sort((a, b) => a.localeCompare(b));
 
-    const eventName = eventFolder.replace(/\\/g, '/').split('/').pop();
+    console.log('Event Files:', eventFiles); // Debug: List event files
+    const eventName = path.basename(eventFolder);
+    console.log('Event Name:', eventName); // Debug: Event name
 
-    client.on(eventName, async (arg) => {
-      for (const eventFile of eventFiles) {
-        const eventFunction = require(eventFile);
-        await eventFunction(client, arg);
+    client.once(eventName, async (arg) => {
+      try {
+        console.log(`Event triggered: ${eventName}`); // Debug: Event trigger
+        for (const eventFile of eventFiles) {
+          const eventFilePath = path.resolve(eventFile);
+          console.log(`Executing file: ${eventFilePath}`); // Debug: Executing file
+          const eventFunction = require(eventFilePath);
+          await eventFunction(client, arg);
+        }
+      } catch (error) {
+        console.error(`Error in event function for event: ${eventName}`, error);
       }
     });
   }
